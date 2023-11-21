5e4cc02adf0beb6f1ab779bb31ee31bd371fca45
montaniaRoja <adolfocp1972@gmail.com>
2023-11-21 15:40:49 -0600

    se agrego listener a mapview para abrir google maps

--- app/src/main/java/com/example/examen2parcial/VisualizarLocalizacion.java
+++ app/src/main/java/com/example/examen2parcial/VisualizarLocalizacion.java
@@ -3,7 +3,9 @@ package com.example.examen2parcial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,6 +20,7 @@ public class VisualizarLocalizacion extends AppCompatActivity {
     private GoogleMap mMap;
     EditText txtLat;
     EditText txtLong;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,21 +32,37 @@ public class VisualizarLocalizacion extends AppCompatActivity {
         mapView.onCreate(savedInstanceState);
         mapView.onResume(); // Necesario para que el mapa se muestre de inmediato
 
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                // El mapa está listo
-                mMap = googleMap;
+        mapView.getMapAsync(googleMap -> {
+            // El mapa está listo
+            mMap = googleMap;
 
-                // Obtener la latitud y longitud de los extras
-                Intent intent = getIntent();
-                double latitud = Double.parseDouble(intent.getStringExtra("latitud"));
-                double longitud = Double.parseDouble(intent.getStringExtra("longitud"));
-                txtLat.setText(String.valueOf(latitud));
-                txtLong.setText(String.valueOf(longitud));
-                LatLng ubicacion = new LatLng(latitud, longitud);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
-                mMap.addMarker(new MarkerOptions().position(ubicacion).title("Marcador en la ubicación"));
+            // Obtener la latitud y longitud de los extras
+            Intent intent = getIntent();
+            double latitud = Double.parseDouble(intent.getStringExtra("latitud"));
+            double longitud = Double.parseDouble(intent.getStringExtra("longitud"));
+            txtLat.setText(String.valueOf(latitud));
+            txtLong.setText(String.valueOf(longitud));
+            LatLng ubicacion = new LatLng(latitud, longitud);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+            mMap.addMarker(new MarkerOptions().position(ubicacion).title("Marcador en la ubicación"));
+        });
+
+        mapView.setOnClickListener(v -> {
+            // Obtener la latitud y longitud de los extras
+            Intent intent = getIntent();
+            double latitud = Double.parseDouble(intent.getStringExtra("latitud"));
+            double longitud = Double.parseDouble(intent.getStringExtra("longitud"));
+
+            // Crear una Uri con la ubicación
+            Uri gmmIntentUri = Uri.parse("geo:" + latitud + "," + longitud + "?q=" + latitud + "," + longitud + "(Ubicación)");
+
+            // Crear un Intent para abrir Google Maps
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+
+            // Verificar si hay una aplicación que pueda manejar el Intent
+            if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(mapIntent);
             }
         });
     }
